f6609b5894267c17642a36023339cbafba1df4cc
Dung2781993@gmail.com <Dung2781993@gmail.com>
2020-06-12 01:36:46 +1000

    Update route

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ import schema from './schema';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('GraphQL is amazing!');
-});
+const root = { hello: () => "Hi, I'm Manny" };
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}))
 
 app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
