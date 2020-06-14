4c7cb1e2e4d3ef6524a8bc1e52cee1eda97340d2
Dung2781993@gmail.com <Dung2781993@gmail.com>
2020-06-14 21:41:05 +1000

    Update resolver

--- index.js
+++ index.js
@@ -4,19 +4,37 @@ import schema from './schema';
 
 const app = express();
 
-const root = { friend: () => {
-    return {
-        "id" : 1993,
-        "firstName": 'Hellomama',
-        "lastName": 'Adam',
-        "gender": 'Male',
-        "language": "English",
-        "emails": [
-            {email: "Hellomama93@gmail.com"}, 
-            {email: "Hellomama@gmail.com"} 
-        ]
+class Friend {
+    constructor(id, {firstName, lastName, gender, language, email}) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.language = language;
+        this.email = email;
     }
-}};
+}
+
+const friendDatabase = {};
+
+
+const root = { friend: () => {
+        return {
+            "id" : 1993,
+            "firstName": 'Hellomama',
+            "lastName": 'Adam',
+            "gender": 'Male',
+            "language": "English",
+            "emails": [
+                {email: "Hellomama93@gmail.com"}, 
+                {email: "Hellomama@gmail.com"} 
+            ]
+        }
+    },
+    createFriend: ({input}) =>{
+        let id = require('crypto').randomBytes(10).toString('hex');
+    }
+};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
